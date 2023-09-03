42d50495775ae791ef9d428a4ba681af507b34c5
Sergi Cortés <sergi.cortes@nextmol.com>
2023-09-03 11:10:53 +0200

    Fix neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -57,6 +57,15 @@ return {
                 desc = "Explorer NeoTree (document_symbols)"
             }
         },
+        deactivate = function() vim.cmd([[Neotree close]]) end,
+        init = function()
+            if vim.fn.argc() == 1 then
+                local stat = vim.loop.fs_stat(vim.fn.argv(0))
+                if stat and stat.type == "directory" then
+                    require("neo-tree")
+                end
+            end
+        end,
         opts = {
             sources = {
                 "filesystem", "buffers", "git_status", "document_symbols"
