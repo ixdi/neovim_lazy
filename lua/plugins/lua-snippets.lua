4c79d9bb49ae5edc5a1c5b8fba08d325bf4bc8b2
Sergi Cortés <sergi.cortes@nextmol.com>
2023-12-02 17:57:41 +0100

    Update luasnip

--- lua/plugins/lua-snippets.lua
+++ lua/plugins/lua-snippets.lua
@@ -2,16 +2,15 @@ return {
 	-- snippets
 	{
 		"L3MON4D3/LuaSnip",
-		build = (not jit.os:find("Windows"))
-				and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-			or nil,
+		version = "v2.*",
+		build = "make install_jsregexp",
 		dependencies = {
 			"rafamadriz/friendly-snippets",
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-				require("luasnip/loaders/from_snipmate").lazy_load()
-			end,
 		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_snipmate").lazy_load()
+		end,
 		opts = { history = true, delete_check_events = "TextChanged" },
         -- stylua: ignore
         keys = {
