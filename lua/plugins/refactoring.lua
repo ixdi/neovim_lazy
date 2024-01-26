6ff06fa2f873ab02cc6311a141317f057ce92588
Sergi Cortés <sergi.cortes@nextmol.com>
2024-01-26 07:39:22 +0100

    fix refactoring

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -5,39 +5,52 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
 		},
-		lazy = true,
-		opts = {
-			prompt_func_return_type = {
-				c = false,
-				cpp = true,
-				cxx = false,
-				go = false,
-				h = false,
-				hpp = false,
-				java = false,
-				js = true,
-				ts = true,
-				py = true,
-			},
-			prompt_func_param_type = {
-				c = false,
-				cpp = true,
-				cxx = false,
-				go = false,
-				h = false,
-				hpp = false,
-				java = false,
-				js = true,
-				ts = true,
-				py = true,
-			},
-			printf_statements = {},
-			print_var_statements = {},
-		},
 		config = function()
-			require("refactoring").setup()
+			require("refactoring").setup({
+				prompt_func_return_type = {
+					c = false,
+					cpp = true,
+					cxx = false,
+					go = false,
+					h = false,
+					hpp = false,
+					java = false,
+					js = true,
+					ts = true,
+					py = true,
+				},
+				prompt_func_param_type = {
+					c = false,
+					cpp = true,
+					cxx = false,
+					go = false,
+					h = false,
+					hpp = false,
+					java = false,
+					js = true,
+					ts = true,
+					py = true,
+				},
+				printf_statements = {},
+				print_var_statements = {},
+			})
 			-- load refactoring Telescope extension
 			require("telescope").load_extension("refactoring")
+			vim.keymap.set({ "n", "x" }, "<leader>rr", function()
+				require("telescope").extensions.refactoring.refactors()
+			end)
+			-- Automated insertion of print statement to mark the calling of a function
+			vim.keymap.set("n", "<leader>rp", function()
+				require("refactoring").debug.printf({ below = false })
+			end)
+			-- Print variables
+			vim.keymap.set({ "x", "n" }, "<leader>rv", function()
+				require("refactoring").debug.print_var()
+			end)
+			-- Cleanup prints
+			vim.keymap.set("n", "<leader>rc", function()
+				require("refactoring").debug.cleanup({})
+			end)
 		end,
 	},
 }
