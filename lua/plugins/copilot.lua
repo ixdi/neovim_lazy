adbbbc0515a51fa2d146e129400e98580b4833f4
Sergi Cortés <sergi_cortes@hotmail.com>
2024-06-03 13:53:39 +0200

    update copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -38,6 +38,7 @@ return {
 					javascript = true, -- allow specific filetype
 					typescript = true, -- allow specific filetype
 					typescriptreact = true, -- allow specific filetype
+					python = true,
 					gitcommit = true,
 					markdown = true,
 					["."] = false, -- disable for all other filetypes and ignore default `filetypes`
