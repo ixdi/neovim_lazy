535f5706e055c21b69434b5cac96c4d7b2e5d07d
Sergi Cortés <sergi_cortes@hotmail.com>
2024-07-14 15:57:44 +0200

    change surround to sandwich

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,11 @@
+return {
+	--   surr*ound_words             saiw)                     (surround_words)
+	--   *make strings               sa$"                      "make strings"
+	--   [delete ar*ound me!]        sd]                       delete around me!
+	--   remove <b>HTML t*ags</b>    sd<b>                     remove HTML tags
+	--   'change quot*es'            sr'"                      "change quotes"
+	--   or tag* types               select visual -> S<h1>    <h1>or tag types</h1>
+	{
+		"machakann/vim-sandwich",
+	},
+}
