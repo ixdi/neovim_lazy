1e5319697f0b5fdb14aeb7c5bfdb0344923fcc0f
Sergi Cortés <sergi_cortes@hotmail.com>
2024-10-10 09:06:10 +0200

    Add superhtml

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -182,6 +182,16 @@ return {
 						vim.api.nvim_win_set_cursor(0, curpos)
 					end,
 				})
+				vim.api.nvim_create_autocmd("Filetype", {
+					pattern = { "html", "shtml", "htm" },
+					callback = function()
+						vim.lsp.start({
+							name = "superhtml",
+							cmd = { "superhtml", "lsp" },
+							root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
+						})
+					end,
+				})
 			end
 
 			-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
