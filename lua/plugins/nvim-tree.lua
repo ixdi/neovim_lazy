1cf886619e66308b14877365b2124a2441ded694
Sergi Cortés <sergi_cortes@hotmail.com>
2025-11-18 11:14:01 +0100

    fix: nvim tree close when is the last

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"nvim-tree/nvim-tree.lua",
 		version = "*",
-		lazy = false,
+		lazy = true,
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
@@ -32,6 +32,27 @@ return {
 				filters = {
 					dotfiles = false,
 				},
+				renderer = {
+					decorators = {
+						"Git",
+						"Open",
+						"Hidden",
+						"Modified",
+						"Bookmark",
+						"Diagnostics",
+						"Copied",
+						"Cut",
+					},
+				},
+			})
+
+			vim.api.nvim_create_autocmd("BufEnter", {
+				nested = true,
+				callback = function()
+					if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+						vim.cmd("quit")
+					end
+				end,
 			})
 		end,
 		keys = {
